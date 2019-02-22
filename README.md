51d8b9c2cbbd859bb607f31906be08b0616c4428
Draper_HXY <draper_hxy@163.com>
2019-02-22 21:25:15 +0800

    feat Exist BUG!!!
    官方提倡使用 zipkin.jar，而并非自己搭建
    但是就想自己搭建 ZipKin Server
    搭建 ZipKin Server 再次失败
    但是将 zipkin.jar 上传上去

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # itoken-zipkin
+
+* [链路追踪配置修改](https://segmentfault.com/a/1190000017031169?utm_source=tag-newest)
---
+++ src/main/java/com/draper/itoken/zipkin/ZipKinApplication.java
@@ -0,0 +1,15 @@
+package com.draper.itoken.zipkin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@SpringBootApplication
+@EnableEurekaClient
+public class ZipKinApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZipKinApplication.class, args);
+    }
+
+}
